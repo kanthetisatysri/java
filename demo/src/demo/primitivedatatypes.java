53abff710b52537f4c5f3f009a168603906a4ebc
acer <acer@192.168.0.106>
2022-06-27 14:30:28 +0530

    PRIMITIVE DATATYPES

---
+++ demo/src/demo/primitivedatatypes.java
@@ -0,0 +1,48 @@
+package demo;
+
+public class primitivedatatypes
+{
+	public static void main(String[] args)
+
+{
+int a = 22;
+int b = 33;
+int c = a+b;
+System.out.println("GIVE THE VALUE OF c:" +c);
+{
+int value1 = 23;
+float value2 = 2/5;
+double value3 = 12f/3f;
+System.out.println("GIVE THE VALUE OF value1:" +value1);
+System.out.println("GIVE THE VALUE OF value2:" +value2 );
+System.out.println("GIVE THE VALUE OF value3:" +value3 );
+{
+boolean var=true;
+System.out.println("value is var:" +var);
+{
+char var1 = 2234;
+	System.out.println("var1");
+	{
+		String var2 = new String("YESUBABU SITA VENKATAMAHESH SATYASRI ");
+		System.out.println("PRINT VAR2:" +var2);
+	}
+	
+	
+	
+	
+	
+	
+	
+}}}}}
+	
+	
+	
+
+	
+	
+	
+
+
+				
+				
+
