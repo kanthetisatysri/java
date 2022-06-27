98387778ddab92eaa1acf784e3c15c9277a780a3
acer <acer@192.168.0.106>
2022-06-27 14:28:54 +0530

    ASSIGNMENT OPERATOR

---
+++ demo/src/demo/Assignmentoperator.java
@@ -0,0 +1,15 @@
+package demo;
+
+public class Assignmentoperator {
+	public static void main(String[] args)
+	{
+		int m=4;
+		m +=9;
+		m -=9;
+		m *=9;
+		m /=9;
+		m %=9;
+		System.out.println(m);
+	}
+
+}
