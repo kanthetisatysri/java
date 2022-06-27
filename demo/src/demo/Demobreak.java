418824e8aab0e991e3cd627c3cd0c270f1be3db6
acer <acer@192.168.0.106>
2022-06-27 14:30:00 +0530

    BREAK AND CONTINUE

---
+++ demo/src/demo/Demobreak.java
@@ -0,0 +1,17 @@
+package demo;
+
+public class Demobreak {
+
+	public static void main(String[] args) {
+		for(int i=2;i<=10;i++)
+		{
+			if( i== 2)
+			{
+				continue;
+			}
+			System.out.println("satyasri");
+			}
+		}
+	}
+
+
