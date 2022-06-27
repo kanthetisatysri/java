651e0a415387950b6ee0d551dfe0141662ae583f
acer <acer@192.168.0.106>
2022-06-27 14:29:28 +0530

    SWITCH DECISION MAKING

---
+++ demo/src/demo/Demoswitch.java
@@ -0,0 +1,21 @@
+package demo;
+
+public class Demoswitch {
+
+	public static void main(String[] args)
+	{
+int day = 2;
+switch (day)
+{
+	case 1:
+		System.out.println("monday");
+		break;
+	case 2:
+		System.out.println("tuesday");
+		break;
+	case 3:
+		System.out.println("wednesday");
+		break;
+}}}
+	
+   
