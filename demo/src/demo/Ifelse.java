563c9da3e999fd777783473b7b7a74e1ed737e8b
acer <acer@192.168.0.106>
2022-06-27 14:25:23 +0530

    DECISION MAKING IF

---
+++ demo/src/demo/Ifelse.java
@@ -0,0 +1,21 @@
+package demo;
+
+public class Ifelse{
+	public static void main (String[] args) { 
+		int n=8;
+		int m=5;
+		if(n>m)
+		{
+			System.out.println(" THIS VALUE IS HIGHEST");
+		}
+		else
+		{
+			System.out.println(" THIS VALUE IS SMALLEST");
+		}
+		
+		
+		
+		
+	}
+
+}
---
+++ demo/src/demo/Nestedifelse.java
@@ -0,0 +1,24 @@
+package demo;
+
+public class Nestedifelse {
+	public static void main (String[] args) {
+		int a=40;
+		if  (a>0) 
+		{
+			if(a%2==0)
+			{
+				System.out.println("a is divisible by 2");
+			}				
+			System.out.println("a is not divisible by 2");
+		}
+		else
+			
+			{
+				System.out.println("a is not a number");
+			}}
+		
+			}
+	
+
+
+
